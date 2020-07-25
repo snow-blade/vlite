cb6bb69ee06a2df11cb1e42774ebcb59baf11da2
snow-blade <freemanirabaruta@gmail.com>
2020-07-25 21:31:30 +0200

    trying to make the code more readable

--- test.v
+++ test.v
@@ -15,24 +15,24 @@ struct Plug {
 fn main(){
 mut args := vargs.new(os.args, 1)
 args.parse()
-file := os.read_file("conf2.json") or {println("error: \nfailed to read the file") return }
-f:=json.decode(Plug,file) or {
+json_file := os.read_file("conf2.json") or {println("error: \nfailed to read the file") return }
+decoded:=json.decode(Plug,json_file) or {
    println(term.red('error: \nfailed to open or parse the \' conf.json\' file'))
    return
 }
-plugs :=f.plugins.split(",")
+plugins_array :=decoded.plugins.split(",")
 
 match args.command
 {
    //i encountered a little problem importing this from utils.v so, i decided to let it as it is.
 "get"{   
-   if args.options['p'] in plugs{
+   if args.options['p'] in plugins_array{
    req:=http.get("https://raw.githubusercontent.com/rxi/lite-plugins/master/plugins/"+args.options['p']+".lua") or {
       println(term.bold(term.red("error: \nfailed to connect to the enpoint, check that your connection is open")))
       return
    }
-   mut fi:= os.create(f.lite_path+"data/plugins/"+args.options['p']+'.lua') or {
-      println("failed to create "+f.lite_path+"data/plugins/"+args.options['p']+'.lua')
+   mut fi:= os.create(decoded.lite_path+"data/plugins/"+args.options['p']+'.lua') or {
+      println("failed to create "+decoded.lite_path+"data/plugins/"+args.options['p']+'.lua')
       return 
    }
    fi.write(req.text) 
@@ -42,10 +42,10 @@ match args.command
    }
 }
 "list"{
-   lib.list_plugins(plugs)
+   lib.list_plugins(plugins_array)
  }
 "search"{
-    lib.search(args.options['p'],plugs)
+    lib.search(args.options['p'],plugins_array)
 }
 "help"{
    lib.help()
