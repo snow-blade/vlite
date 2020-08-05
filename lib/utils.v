e3d8ac0edc24086c563514c505c80c004ee81a95
snow-blade <freemanirabaruta@gmail.com>
2020-08-05 21:21:38 +0200

    applied v fmt on all the files

--- lib/utils.v
+++ lib/utils.v
@@ -1,9 +1,10 @@
 module lib
+
 import term
 import time
 
 pub fn banner() string {
-   return "
+        return '
 ██╗   ██╗██╗     ██╗████████╗███████╗
 ██║   ██║██║     ██║╚══██╔══╝██╔════╝
 ██║   ██║██║     ██║   ██║   █████╗  
@@ -12,47 +13,43 @@ pub fn banner() string {
   ╚═══╝  ╚══════╝╚═╝   ╚═╝   ╚══════╝
              
    _  _   _  | 
-a (_ (_) (_) | package manager for the lite text editor  \n                                "      
- 
+a (_ (_) (_) | package manager for the lite text editor  \n                                '
 }
-
 
 pub fn list_plugins(plugs []string) {
-   println(term.green("All plugins:"))
-   print("")
-   for el in plugs{
-      println(term.cyan("- "+el))
-   }
+        println(term.green('All plugins:'))
+        print('')
+        for el in plugs {
+                println(term.cyan('- ' + el))
+        }
 }
 
-pub fn search(pack string,packs []string){
-     if pack in packs{
-        print(term.bold(term.green("Package "+pack+" exists")))
-     }
-     else{
-        println(term.bold(term.red("Package "+pack+" does not exist in remote repo")))
-        for el in packs{
-           if pack in el{
-              println(term.yellow("Suggesting: "+el))
-              time.sleep(1)
-            }
-           
+pub fn search(pack string, packs []string) {
+        if pack in packs {
+                print(term.bold(term.green('Package ' + pack + ' exists')))
+        } else {
+                println(term.bold(term.red('Package ' + pack + ' does not exist in remote repo')))
+                for el in packs {
+                        if pack in el {
+                                println(term.yellow('Suggesting: ' + el))
+                                time.sleep(1)
+                        }
+                }
         }
-     }
 }
 
 pub fn help() {
-   print(term.bold(term.green(banner())))
-   println("")
-   println(term.green("help :"))
-   println("|")
-   println(term.yellow("+----> Get a package:  ./vlite get -p <Name of the package> "))
-   println("|                                                                   +")
-   println(term.yellow("+----> List packages: ./vlite list"))
-   println("|                                                                   +")
-   println(term.yellow("+----> Get help: ./vlite help"))
-   println("|                                                                   +")
-   println(term.yellow("+----> Search for a package: ./vlite search -p <Name of the package>"))
-   println("|                                                                   +")
-   println(term.yellow("+-------------------------------------------------------------------+"))  
+        print(term.bold(term.green(banner())))
+        println('')
+        println(term.green('help :'))
+        println('|')
+        println(term.yellow('+----> Get a package:  ./vlite get -p <Name of the package> '))
+        println('|                                                                   +')
+        println(term.yellow('+----> List packages: ./vlite list'))
+        println('|                                                                   +')
+        println(term.yellow('+----> Get help: ./vlite help'))
+        println('|                                                                   +')
+        println(term.yellow('+----> Search for a package: ./vlite search -p <Name of the package>'))
+        println('|                                                                   +')
+        println(term.yellow('+-------------------------------------------------------------------+'))
 }
--- vlite.v
+++ vlite.v
@@ -6,58 +6,60 @@ import term
 import lib
 
 struct Plug {
-   version string
-   author string
-   lite_path string
-   plugins string
+        version   string
+        author    string
+        lite_path string
+        plugins   string
 }
 
-fn main(){
-   mut args := vargs.new(os.args, 1)
-   args.parse()
-   json_file := os.read_file("conf.json") or {println("error: \nfailed to read the file") return }
-   decoded:=json.decode(Plug,json_file) or {
-      println(term.red('error: \nfailed to open or parse the \' conf.json\' file'))
-      return
-   }
-   plugins_array :=decoded.plugins.split(",")
-
-   match args.command
-   {
-      //i encountered a little problem importing this from utils.v so, i decided to let it as it is.
-   "get"{   
-      if args.options['p'] in plugins_array{
-            req:=http.get("https://raw.githubusercontent.com/rxi/lite-plugins/master/plugins/"+args.options['p']+".lua") or {
-                 println(term.bold(term.red("error: \nfailed to connect to the enpoint, check that your connection is open")))
-                 return
-            }
-            mut fi:= os.create(decoded.lite_path+"data/plugins/"+args.options['p']+'.lua') or {
-               println("failed to create "+decoded.lite_path+"data/plugins/"+args.options['p']+'.lua')
-              return 
-            }
-            fi.write(req.text) 
-            print(req.text)
-            }
-      else{
-         println(term.bold(term.red("Error:\nPackage not found,")))
-         lib.help()
+fn main() {
+        mut args := vargs.new(os.args, 1)
+        args.parse()
+        json_file := os.read_file('conf.json') or {
+                println('error: \nfailed to read the file')
+                return
         }
-      }
-   "list"{
-         lib.list_plugins(plugins_array)
-      }
-   "search"{
-         lib.search(args.options['p'],plugins_array)
-      }
-   "help"{
-         lib.help()
-      }
-   "banner"{
-        print(lib.banner())
-   }
-   else{
-      println(term.bold(term.red("error: \nCommand not found")))
-      lib.help()
-      }
-   }
+        decoded := json.decode(Plug, json_file) or {
+                println(term.red("error: \nfailed to open or parse the \' conf.json\' file"))
+                return
+        }
+        plugins_array := decoded.plugins.split(',')
+        match args.command {
+                // i encountered a little problem importing this from utils.v so, i decided to let it as it is.
+                'get' {
+                        if args.options['p'] in plugins_array {
+                                req := http.get('https://raw.githubusercontent.com/rxi/lite-plugins/master/plugins/' +
+                                        args.options['p'] + '.lua') or {
+                                        println(term.bold(term.red('error: \nfailed to connect to the enpoint, check that your connection is open')))
+                                        return
+                                }
+                                mut fi := os.create(decoded.lite_path + 'data/plugins/' + args.options['p'] + '.lua') or {
+                                        println('failed to create ' + decoded.lite_path + 'data/plugins/' +
+                                                args.options['p'] + '.lua')
+                                        return
+                                }
+                                fi.write(req.text)
+                                print(req.text)
+                        } else {
+                                println(term.bold(term.red('Error:\nPackage not found,')))
+                                lib.help()
+                        }
+                }
+                'list' {
+                        lib.list_plugins(plugins_array)
+                }
+                'search' {
+                        lib.search(args.options['p'], plugins_array)
+                }
+                'help' {
+                        lib.help()
+                }
+                'banner' {
+                        print(lib.banner())
+                }
+                else {
+                        println(term.bold(term.red('error: \nCommand not found')))
+                        lib.help()
+                }
+        }
 }
